382c0b802d72ab74b2f83d486c27981346a2ec6f
Ishan <ishanwijes@gmail.com>
2022-08-17 18:23:44 +1000

    added environment variable file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,39 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// import sequelize
+const {hasMany, BelongsTo, BelongsToMany} = require('sequelize')
+
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  through: 'product_tag_table',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag_table',
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag_table',
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
+
+
 
 module.exports = {
   Product,
